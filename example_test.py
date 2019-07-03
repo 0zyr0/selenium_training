6d5792bba31527672194ba9f7f1d88101402c72d
zms@innovacia.ru <zms@innovacia.ru>
2019-07-03 17:47:34 +0700

    Добавление простых действий с помощью Selenium

--- example_test.py
+++ example_test.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver(request):
-    # создание драйвера
+    # создание драйвера. Инициализация браузера
     wd = webdriver.Chrome()
     request.addfinalizer(wd.quit)
     return wd
@@ -16,6 +16,10 @@ def test_example(driver):
     driver.get("http://www.google.com/")
     # 10-секундное ожидание
     wait = WebDriverWait(driver, 10)
+    driver.find_element_by_class_name("xPnqmf").click()
+    driver.find_element_by_id("K32").click()
+    #driver.find_element_by_class_name("xPnqmf").click()
     driver.find_element_by_name("q").send_keys("webdriver")
-    driver.find_element_by_name("btnG").click()
+    driver.refresh()
+    driver.find_element_by_name("btnK").click()
     wait.until(EC.title_is("webdriver - Поиск в Google"))
