dd275bbf5244c6600898be33de79feaedfc251c4
zms@innovacia.ru <zms@innovacia.ru>
2019-07-16 09:12:43 +0700

    Задание 8.

--- task8_stickers.py
+++ task8_stickers.py
@@ -2,20 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
 
-
-def is_element_present(driver, *args):
-    try:
-        driver.find_element(*args)
-        return True
-    except NoSuchElementException:
-        return False
-
-
-def are_elements_present(driver, *args):
-    return len(driver.find_elements(*args)) > 0
 
 
 @pytest.fixture
@@ -36,27 +23,15 @@ def test_login(driver):
     #wait.until(EC.title_is("My Store"))
 
 
-def check_exists_by_xpath(xpath):
-    return len(webdriver.find_elements_by_xpath(xpath)) > 0
-
-
-
 def test_check_stickers(driver):
     test_login(driver)
     driver.get("http://localhost/litecart/en/")
 
-    lists = driver.find_elements_by_css_selector("div#box-most-popular li")
+    elements = driver.find_elements_by_css_selector("div.content div.box div.image-wrapper")
 
-    for list in lists:
-        sticker = list.find_element_by_xpath(".//div[@class='image-wrapper']/div")
-        print(sticker)
-        #sticker.get_attribute()
-        # sticker = list.find_element_by_xpath(".//div[@class='sticker new']/")
-    #    lists.get_attribute("div.image-wrapper div")[list]
+    for i, element in enumerate(elements):
+        element = driver.find_elements_by_tag_name("div.sticker")[i]
+        # element = driver.find_element_by_css_selector("div.sticker")
+        assert element
+        print(element)
 
-    #driver.find_elements_by_css_selector("div.box-most-popular li").get_attribute()
-    #check_exists_by_xpath('.//*[@id="box-most-popular"]/div/ul/li[1]/a[1]/div[1]/div')
-
-    # driver.find_element_by_name("username").send_keys("admin")
-    # driver.find_element_by_name("password").send_keys("admin")
-    # driver.find_element_by_name("login").click()
