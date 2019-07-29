0267a0ac1eef3543b9321c4266201d52986ea897
zms@innovacia.ru <zms@innovacia.ru>
2019-07-29 17:53:49 +0700

    Задание 9. Сортировка 50%

--- task9_sort_country.py
+++ task9_sort_country.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
 
 
@@ -27,13 +28,31 @@ def test_check_sort1(driver):
     test_login(driver)
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
 
-    elements = driver.find_elements_by_css_selector("div.content div.box div.image-wrapper")
+    elements = driver.find_elements_by_css_selector("tr.row")
+
+    original_list_country_names = []
+
+    sorted_list_country_names = []
 
     for i, element in enumerate(elements):
-        element = driver.find_elements_by_tag_name("div.sticker")[i]
-        # element = driver.find_element_by_css_selector("div.sticker")
-        assert element
-        print(element)
+        original_list_country_names.append(re.match(r'^\d+\s*\w{2}\s*(.+)\d+$', element.text)[1].strip())
+
+        sorted_list_country_names.append(re.match(r'^\d+\s*\w{2}\s*(.+)\d+$', element.text)[1].strip())
+
+    print(original_list_country_names, '\n')
+
+    sorted_list_country_names.sort()
+
+    print(sorted_list_country_names, '\n')
+
+    if original_list_country_names[i] == sorted_list_country_names[i]:
+        print('Countries sorted by Aa..Zz', '\n')
+    else:
+        print(original_list_country_names[i], ' not equals ', sorted_list_country_names[i], '\n')
+
+
+
+    print('Test Sort Country names has done')
 
 
 def test_check_sort2(driver):
