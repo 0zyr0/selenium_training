89a9e88fe966cea9a461b92a05ee8b925d23ff4a
zms@innovacia.ru <zms@innovacia.ru>
2019-08-05 14:32:56 +0700

    Задача 9. Ликвидация дублируемого кода

--- task9_sort_country.py
+++ task9_sort_country.py
@@ -52,26 +52,45 @@ def loop_for_zones(el, o_list, sorted_list):
         sorted_list.append(element3.get_property('text'))
 
 
+def loop_for_country(original_list_country, sorted_list_country, elements):
+
+    for i, element in enumerate(elements):
+        original_list_country.append(re.match(r'^\d+\s*\w{2}\s*(.+)\d+$', element.text)[1].strip())
+
+        sorted_list_country.append(re.match(r'^\d+\s*\w{2}\s*(.+)\d+$', element.text)[1].strip())
+
+
+def make_some_sorting(elements_z, z_list, s_list):
+    loop_for_zones(elements_z, z_list, s_list)
+
+    s_list.sort()
+
+    checking_sorting(z_list, s_list)
+
+
+def sorting_zones(zone_element, o_list_zone, s_list_zone, length_el):
+    loop_for_zones_edit(zone_element, o_list_zone, s_list_zone, length_el)
+
+    s_list_zone.sort()
+
+    checking_sorting(o_list_zone, s_list_zone)
+
+
 def test_check_sort1(driver):
     test_login(driver)
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
 
-    elements = driver.find_elements_by_css_selector("tr.row")
+    element_country = driver.find_elements_by_css_selector("tr.row")
 
     original_list_country_names = []
 
     sorted_list_country_names = []
 
-    for i, element in enumerate(elements):
-        original_list_country_names.append(re.match(r'^\d+\s*\w{2}\s*(.+)\d+$', element.text)[1].strip())
-
-        sorted_list_country_names.append(re.match(r'^\d+\s*\w{2}\s*(.+)\d+$', element.text)[1].strip())
+    loop_for_country(original_list_country_names, sorted_list_country_names, element_country)
 
     sorted_list_country_names.sort()
 
     checking_sorting(original_list_country_names, sorted_list_country_names)
-
-    # driver.get("http://localhost/litecart/admin/?app=countries&doc=edit_country&country_code=CA")
 
     driver.find_element_by_tag_name(
         "a[href='http://localhost/litecart/admin/?app=countries&doc=edit_country&country_code=CA'][title='Edit']").click()
@@ -84,13 +103,7 @@ def test_check_sort1(driver):
 
     length_of_elements = len(element_2) - 2
 
-    loop_for_zones_edit(element_2, original_list_zones_ca, sorted_list_zones_ca, length_of_elements)
-
-    sorted_list_zones_ca.sort()
-
-    checking_sorting(original_list_zones_ca, sorted_list_zones_ca)
-
-    # driver.get("http://localhost/litecart/admin/?app=countries&doc=edit_country&country_code=US")
+    sorting_zones(element_2, original_list_zones_ca, sorted_list_zones_ca, length_of_elements)
 
     driver.find_element_by_tag_name("li[class='selected']").click()
 
@@ -105,18 +118,12 @@ def test_check_sort1(driver):
 
     length_of_elements_2 = len(element_3) - 2
 
-    loop_for_zones_edit(element_3, original_list_zones_us, sorted_list_zones_us, length_of_elements_2)
-
-    sorted_list_zones_us.sort()
-
-    checking_sorting(original_list_zones_us, sorted_list_zones_us)
+    sorting_zones(element_3, original_list_zones_us, sorted_list_zones_us, length_of_elements_2)
 
 
 def test_check_sort2(driver):
     test_login(driver)
     driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
-
-    # driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=edit_geo_zone&page=1&geo_zone_id=1")
 
     driver.find_element_by_tag_name(
         "a[href='http://localhost/litecart/admin/?app=geo_zones&doc=edit_geo_zone&page=1&geo_zone_id=1'][title='Edit']").click()
@@ -125,13 +132,7 @@ def test_check_sort2(driver):
     list3 = []
     sorted_list3 = []
 
-    loop_for_zones(el3, list3, sorted_list3)
-
-    sorted_list3.sort()
-
-    checking_sorting(list3, sorted_list3)
-
-    # driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=edit_geo_zone&page=1&geo_zone_id=2")
+    make_some_sorting(el3, list3, sorted_list3)
 
     driver.find_element_by_tag_name("li[class='selected']").click()
 
@@ -142,8 +143,4 @@ def test_check_sort2(driver):
     list4 = []
     sorted_list4 = []
 
-    loop_for_zones(el4, list4, sorted_list4)
-
-    sorted_list4.sort()
-
-    checking_sorting(list4, sorted_list4)
+    make_some_sorting(el4, list4, sorted_list4)
