0afbc3242b0c567f388e3ce5e42ccffa0c18a462
zms@innovacia.ru <zms@innovacia.ru>
2019-08-06 17:58:21 +0700

    Task 17. 20%

--- task17_logs_in_browser.py
+++ task17_logs_in_browser.py
@@ -3,6 +3,43 @@
 #
 # Сценарий должен состоять из следующих частей:
 #
-# 1) зайти в админку
-# 2) открыть каталог, категорию, которая содержит товары (страница http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1)
 # 3) последовательно открывать страницы товаров и проверять, не появляются ли в логе браузера сообщения (любого уровня)
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+
+
+@pytest.fixture
+def driver(request):
+    # создание драйвера. Инициализация браузера
+    wd = webdriver.Chrome()
+    # wd = webdriver.Chrome("D:/python_selenium_test/chromedriver_win32/chromedriver.exe")
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_login(driver):
+    driver.get("http://localhost/litecart/admin/")
+    wait = WebDriverWait(driver, 10)
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    # wait.until(EC.title_is("My Store"))
+
+
+def test_log_browser(driver):
+    test_login(driver)
+
+    driver.get("http://localhost/litecart/admin/")
+
+    driver.find_element_by_tag_name("li#app-:nth-child(2)").click()
+
+    driver.find_element_by_tag_name("a[href='http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1']").click()
+
